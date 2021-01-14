11d9130f5b64deade4ece8f4a547aa254a1bec1a
TitoCodes <titosaid2008@gmail.com>
2021-01-13 19:02:26 -0800

    created trajectory with smoke

--- Bird.js
+++ Bird.js
@@ -2,12 +2,22 @@ class Bird extends BaseClass {
   constructor(x,y){
     super(x,y,50,50);
     this.image = loadImage("sprites/bird.png");
+    this.simage = loadImage("sprites/smoke.png");
+    
+    this.traj = [];
   }
 
   display() {
-    //this.body.position.x = mouseX;
-    //this.body.position.y = mouseY;
-
     super.display();
+
+    if(this.body.velocity.x>10&&this.body.position.x>200){
+    var pos = [this.body.position.x, this.body.position.y];
+    this.traj.push(pos);
+    }
+    //this.traj = [[100,200],[110,220],[150,230]];
+    
+    for(var i=0;i<this.traj.length;i++){
+    image(this.simage,this.traj[i][0],this.traj[i][1]);
+    }
   }
 }
